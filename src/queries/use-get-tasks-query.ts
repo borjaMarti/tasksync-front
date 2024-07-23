9225eea690ece3d64c54ee822cde8c3bee9a96b9
Borja Martí <borjamarti@outlook.com>
2024-07-23 14:08:08 +0200

    modified get tasks query typing

--- src/queries/use-get-tasks-query.ts
+++ src/queries/use-get-tasks-query.ts
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/vue-query';
 import type { Task } from '@/types/task.types';
 
-const fetcher = async (): Promise<Task[]> =>
+const fetcher = async () =>
   await fetch('http://localhost:3000/tasks').then((response) => response.json());
 
 export default function useGetTasksQuery() {
-  return useQuery({
+  return useQuery<Task[]>({
     queryKey: ['tasks'],
     queryFn: fetcher,
   });
